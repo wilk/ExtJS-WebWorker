db1180dc02d098b78a63c62a473a1811bd92ba4e
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-01-19 01:39:12 +0100

    added new stable and jshinted version

---
+++ WebWorker.js
@@ -0,0 +1,219 @@
+/**
+ * @class Ext.ux.WebWorker
+ * @author Vincenzo Ferrari <wilk3ert@gmail.com>
+ *
+ * Wrapper for HTML5 WebWorker
+ * 
+ * This class provide an interface for HTML5 WebWorker.
+ *
+ * <h1>Pure text communication</h1>
+ * The communication is text-only, without objects or any other kind of data.
+ *
+ *     var webworker = Ext.create ('Ext.ux.WebWorker', {
+ *       file: 'myWorker.js' ,
+ *       listeners: {
+ *         message: function (ww, message) {
+ *           console.log ('A new message is arrived: ' + message);
+ *         } ,
+ *         error: function (ww, error) {
+ *           Ext.Error.raise (error);
+ *         }
+ *       }
+ *     });
+ *
+ * <h1>Pure event-driven communication</h1>
+ * The communication is event-driven: an event and a String or Object are sent and the webworker handles different events.
+ *
+ *     var webworker = Ext.create ('Ext.ux.WebWorker', {
+ *       file: 'myWorker.js' ,
+ *       listeners: {
+ *         message: function (ww, message) {
+ *           ww.send ('parse', 'a string to parse');
+ *           ww.send ('verify equation', {
+ *             equation: 'x+y-z=10' ,
+ *             x: 10 ,
+ *             y: 5 ,
+ *             z: 5
+ *           });
+ *         }
+ *       }
+ *     });
+ *     
+ *     // A 'terminate' event is sent from the worker (myWorker.js)
+ *     // 'data' has 'log' and 'msg' fields
+ *     webworker.on ('terminate', function (data) {
+ *       console.log ('Log: ' + data.log);
+ *       console.log ('Message: ' + data.msg);
+ *     });
+ *
+ * <h1Mixed communication</h1>
+ * The communication is mixed: it can handles text-only and event-driven communication.
+ *
+ *     var webworker = Ext.create ('Ext.ux.WebWorker', {
+ *       file: 'myWorker.js' ,
+ *       listeners: {
+ *         message: function (ww, message) {
+ *           console.log ('Text-only message arrived is: ' + message);
+ *           
+ *           ww.send ('parse', 'a string to parse');
+ *           ww.send ('verify equation', {
+ *             equation: 'x+y-z=10' ,
+ *             x: 10 ,
+ *             y: 5 ,
+ *             z: 5
+ *           });
+ *         }
+ *       }
+ *     });
+ *     
+ *     // A 'terminate' event is sent from the worker (myWorker.js)
+ *     // 'data' has 'log' and 'msg' fields
+ *     webworker.on ('terminate', function (data) {
+ *       console.log ('Log: ' + data.log);
+ *       console.log ('Message: ' + data.msg);
+ *     });
+ */
+Ext.define ('Ext.ux.WebWorker', {
+	alias: 'widget.webworker' ,
+	
+	mixins: {
+		observable: 'Ext.util.Observable'
+	} ,
+	
+	config: {
+		/**
+		 * @cfg {String} blob Inline worker. Use only this or file.
+		 */
+		blob: '' ,
+		
+		/**
+		 * @cfg {String} file A separated that contains the worker. Use only this or blob.
+		 */
+		file: '' ,
+		
+		/**
+		 * @cfg {String} itemId A further id specified by the user.
+		 */
+		itemId: ''
+	} ,
+	
+	/**
+	 * Creates new WebWorker
+	 * @param {Object} config The configuration options may be specified as follows:
+	 * 
+	 *     var config = {
+	 *       file: 'myWorker.js'
+	 *     };
+	 *     
+	 *     or
+	 *
+	 *     var config = {
+	 *       blob: 'onmessage = function (e) {postMessage ("A message from worker: + " e.data);}'
+	 *     };
+	 *
+	 *     var ww = Ext.create ('Ext.ux.WebWorker', config);
+	 *
+	 * @return {Ext.ux.WebWorker} An instance of Ext.ux.WebWorker or null if an error occurred.
+	 */
+	constructor: function (cfg) {
+		var me = this;
+		
+		me.initConfig (cfg);
+		me.mixins.observable.constructor.call (me, cfg);
+		
+		me.addEvents (
+			/**
+			 * @event error
+			 * Fires after an error occured
+			 * @param {Ext.ux.WebWorker} this The webworker
+			 * @param {Object} error The error object to display
+			 */
+			'error' ,
+			
+			/**
+			 * @event message
+			 * Fires after a message is arrived from the worker.
+			 * @param {Ext.ux.WebWorker} this The webworker
+			 * @param {String/Object} message The message arrived
+			 */ 
+			'message'
+		);
+		
+		try {
+			// Makes inline worker
+			if (Ext.isEmpty (me.getFile ())) {
+				var winURL = window.URL || window.webkitURL ,				
+					blob = new Blob ([me.getBlob ()], {type: 'text/plain'}) ,
+					inlineFile = winURL.createObjectURL (blob);
+				
+				me.worker = new Worker (inlineFile);
+			}
+			// Uses file
+			else {
+				me.worker = new Worker (me.getFile ());
+			}
+			
+			me.id = Ext.id ();
+			
+			me.worker.onmessage = function (message) {
+				// Message event is always sent
+				me.fireEvent ('message', me, message.data);
+				
+				/*
+					message.data : object
+					msg.event : event to raise
+					msg.data : data to handle
+				*/
+				if (Ext.isObject (message.data)) me.fireEvent (message.data.event, me, message.data.data);
+			};
+			
+			me.worker.onerror = function (message) {
+				me.fireEvent ('error', me, message);
+			};
+		}
+		catch (err) {
+			Ext.Error.raise (err);
+			
+			return null;
+		}
+		
+		return me;
+	} ,
+	
+	/**
+	 * @method send
+	 * Sends data. If there's only the first parameter (events), it sends it as a normal string, otherwise as an object
+	 * @param {String/String[]} events Events that have to be handled by the worker
+	 * @param {String/Object} data The data to send
+	 */
+	send: function (events, data) {
+		var me = this;
+		
+		// Treats it as a normal message
+		if (arguments.length === 1) {
+			if (Ext.isString (events)) me.worker.postMessage (events);
+			else Ext.Error.raise ('String expected!');
+		}
+		// Treats it as an event-driven message
+		else if (arguments.length >= 2) {
+			if (Ext.isString (events)) events = [events];
+			
+			Ext.each (events, function (event) {
+				var msg = {
+					event: event ,
+					data: data
+				};
+				
+				me.worker.postMessage (msg);
+			});
+		}
+	} ,
+	
+	/**
+	 * @method stop
+	 * Stops the webworker
+	 */
+	stop: function () {
+		this.worker.terminate ();
+	}
+});
---
+++ WebWorkerManager.js
@@ -0,0 +1,216 @@
+/**
+ * @class Ext.ux.WebWorkerManager
+ * @author Vincenzo Ferrari <wilk3ert@gmail.com>
+ * @singleton
+ * 
+ * Manager of Ext.ux.WebWorker
+ * 
+ * This singleton provide some useful functions to use for many webworkers.
+ *
+ *     var ww1 = Ext.create ('Ext.ux.WebWorker', {
+ *       file: 'myWorker.js'
+ *     });
+ *     
+ *     Ext.ux.WebWorkerManager.register (ww1);
+ *     
+ *     var blob = [
+ *       'onmessage = function (msg) {' ,
+ *       '	if (msg.event === "system shutdown") {' ,
+ *       '		var res = {' ,
+ *       '			event: "stop" ,' ,
+ *       '			data: "Computation of worker 2 terminates correctly in 15h13m20s."' ,
+ *       '		};' ,
+ *       '		postMessage (res);' ,
+ *       '	}' ,
+ *       '}'
+ *     ].join ('');
+ *     
+ *     var ww2 = Ext.create ('Ext.ux.WebWorker', {
+ *       blob: blob
+ *     });
+ *     
+ *     Ext.ux.WebWorkerManager.register (ww2);
+ *     
+ *     var ww3 = Ext.create ('Ext.ux.WebWorker', {
+ *     file: 'otherWorker.js'
+ *     });
+ *     
+ *     Ext.ux.WebWorkerManager.register (ww3);
+ *     
+ *     Ext.ux.WebWorkerManager.listen ('stop', function (ww, data) {
+ *       Ext.Msg.show ({
+ *         title: 'Worker stopped' ,
+ *         msg: 'Log of the computation: ' + data ,
+ *         icon: Ext.Msg.INFO ,
+ *         buttons: Ext.Msg.OK
+ *       });
+ *     });
+ *     
+ *     // This will handled by everyone
+ *     Ext.ux.WebWorkerManager.broadcast ('system shutdown', 'BROADCAST: the system will shutdown in few minutes.');
+ *     
+ *     Ext.ux.WebWorkerManager.stopAll ();
+ *     
+ *     Ext.ux.WebWorkerManager.unregister (ww1);
+ *     Ext.ux.WebWorkerManager.unregister (ww2);
+ *     Ext.ux.WebWorkerManager.unregister (ww3);
+ */
+Ext.define ('Ext.ux.WebWorkerManager', {
+	singleton: true ,
+	
+	/**
+	 * @property {Ext.util.HashMap} wsList
+	 * @private
+	 */
+	wwList: Ext.create ('Ext.util.HashMap') ,
+	
+	/**
+	 * @method register
+	 * Registers one or more Ext.ux.WebWorker
+	 * @param {Ext.ux.WebWorker/Ext.ux.WebWorker[]} webworkers WebWorkers to register. Could be only one.
+	 */
+	register: function (webworkers) {
+		var me = this;
+		
+		if (Ext.isObject (webworkers)) webworkers = [webworkers];
+		
+		Ext.each (webworkers, function (webworker) {
+			me.wwList.add (webworker.id, webworker);
+		});
+	} ,
+	
+	/**
+	 * @method unregister
+	 * Unregisters one or more Ext.ux.WebWorker
+	 * @param {Ext.ux.WebWorker/Ext.ux.WebWorker[]} webworkers WebWorkers to unregister
+	 */
+	unregister: function (webworkers) {
+		var me = this;
+		
+		if (Ext.isObject (webworkers)) webworkers = [webworkers];
+		
+		Ext.each (webworkers, function (webworker) {
+			if (me.wwList.containsKey (webworker.id)) me.wwList.removeAtKey (webworker.id);
+		});
+	} ,
+	
+	/**
+	 * @method contains
+	 * Checks if a webworker is already registered or not
+	 * @param {Ext.ux.WebWorker} webworker The WebWorker to find
+	 * @return {Boolean} True if the webworker is already registered, False otherwise
+	 */
+	contains: function (webworker) {
+		return this.wwList.containsKey (webworker.id);
+	} ,
+	
+	/**
+	 * @method get
+	 * Retrieves a registered webworker by its id
+	 * @param {String} id The id of the webworker to search
+	 * @return {Ext.ux.WebWorker} The webworker or undefined
+	 */
+	get: function (id) {
+		return this.wwList.get (id);
+	} ,
+	
+	/**
+	 * @method getExcept
+	 * Retrieves registered webworkers except the input
+	 * @param {Ext.ux.WebWorker/Ext.ux.WebWorker[]} webworkers WebWorkers to exclude
+	 * @return {Ext.util.HashMap} Registered webworkers except the input
+	 * @private
+	 */
+	getExcept: function (webworkers) {
+		if (Ext.isObject (webworkers)) webworkers = [webworkers];
+		
+		var list = this.wwList.clone ();
+		
+		Ext.each (webworkers, function (webworker) {
+			list.removeAtKey (webworker.id);
+		});
+		
+		return list;
+	} ,
+	
+	/**
+	 * @method each
+	 * Executes a function for each registered webwork
+	 * @param {Function} fn The function to execute
+	 */
+	each: function (fn) {
+		this.wwList.each (function (id, webworker, len) {
+			fn (webworker);
+		});
+	} ,
+	
+	/**
+	 * @method stopAll
+	 * Stops any registered webworker
+	 */
+	stopAll: function () {
+		var me = this;
+		
+		me.wwList.each (function (id, webworker, len) {
+			webworker.stop ();
+			me.unregister (webworker);
+		});
+	} ,
+	
+	/**
+	 * @method listen
+	 * Adds an handler for events given to each registered webworker
+	 * @param {String/String[]} events Events to listen
+	 * @param {Function} handler The events' handler
+	 */
+	listen: function (events, handler) {
+		if (Ext.isString (events)) events = [events];
+		
+		this.wwList.each (function (id, webworker, len) {
+			Ext.each (events, function (event) {
+				webworker.on (event, handler);
+			});
+		});
+	} ,
+	
+	/**
+	 * @method listenExcept
+	 * Adds an handler for events given to each registered webworker, except webworkers given
+	 * @param {String/String[]} events Events to listen
+	 * @param {Ext.ux.WebWorker/Ext.ux.WebWorker[]} webworkers WebWorkers to exclude
+	 * @param {Function} handler The events' handler
+	 */
+	listenExcept: function (events, webworkers, handler) {
+		if (Ext.isString (events)) events = [events];
+		
+		this.getExcept(webworkers).each (function (id, webworker, len) {
+			Ext.each (events, function (event) {
+				webworker.on (event, handler);
+			});
+		});
+	} ,
+	
+	/**
+	 * @method multicast
+	 * Sends a message to each webworker, except those specified
+	 * @param {Ext.ux.WebWorker/Ext.ux.WebWorker[]} webworkers An array of webworkers to take off the communication
+	 * @param {String} event The event to raise
+	 * @param {String/Object} data The data to send
+	 */
+	multicast: function (webworkers, event, data) {
+		this.getExcept(webworkers).each (function (id, webworker, len) {
+			if (Ext.isEmpty (data)) webworker.send (event);
+			else webworker.send (event, data);
+		});
+	} ,
+	
+	/**
+	 * @method broadcast
+	 * Sends a message to each webworker
+	 * @param {String} event The event to raise
+	 * @param {String/Object} data The data to send
+	 */
+	broadcast: function (event, data) {
+		this.multicast ([], event, data);
+	}
+});
